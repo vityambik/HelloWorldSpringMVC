299b5a18ca2f8d672c209f756a60fbd0fddf79fd
Vityambik <vityambik13@gmail.com>
2024-01-25 18:39:40 +0600

    Initial commit

---
+++ src/main/java/com/example/helloworldspringmvc/HelloWorldSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldspringmvc/HelloWorldSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
