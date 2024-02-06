a89e4f3fe4fde864c5afc7e3179032df2d550901
Vityambik <vityambik13@gmail.com>
2024-02-06 13:53:52 +0600

    Add endpoint /greeting

--- src/main/java/com/example/helloworldspringmvc/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworldspringmvc/controller/HelloWorldController.java
@@ -1,2 +1,16 @@
-package com.example.helloworldspringmvc.controller;public class HelloWorldController {
+package com.example.helloworldspringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/greeting")
+    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
